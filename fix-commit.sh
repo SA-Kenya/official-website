339f44558808370ec6d6fda93b5fa13277ccc9af
Mathenge <mathenge2waweru@gmail.com>
2026-06-03 23:33:42 +0300

    Remove temporary commit script.

--- fix-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd /mnt/d/Projects/SA/official-website
-git reset --soft HEAD~1
-git reset HEAD commit.sh 2>/dev/null || true
-rm -f commit.sh .git/COMMIT_MSG_TMP
-git add -A
-git commit -F .git/COMMIT_MSG_TMP2
